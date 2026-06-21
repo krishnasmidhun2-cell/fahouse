8b7eefd525cf663e6f1e82b57beffe5984035250
krishnasmidhun2-cell <krishnasmidhun2@gmail.com>
2026-06-21 13:15:26 +0530

    Add custom App component and global styles import

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+import '../styles/globals.css';
+
+// This default App component initializes the app and loads global styles.
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
